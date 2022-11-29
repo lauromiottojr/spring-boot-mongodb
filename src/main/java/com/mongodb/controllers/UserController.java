9d42567167992ba9bbf8042f49c4c6c6b6ed51e5
lauromiottojr <junior.miotto36@gmail.com>
2022-11-29 15:37:06 -0300

    Using DTO pattern

--- src/main/java/com/mongodb/controllers/UserController.java
+++ src/main/java/com/mongodb/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.mongodb.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.dtos.UserDTO;
 import com.mongodb.models.User;
 import com.mongodb.services.UserService;
 
@@ -19,9 +21,11 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		// convertendo list em listDTO
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
