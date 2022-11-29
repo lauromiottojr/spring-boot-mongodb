00b2301a444da8954fcf7555da12bbd764ef4988
lauromiottojr <junior.miotto36@gmail.com>
2022-11-29 11:39:25 -0300

    Creating Project

---
+++ src/main/java/com/mongodb/WsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMongodbApplication.class, args);
+	}
+
+}
