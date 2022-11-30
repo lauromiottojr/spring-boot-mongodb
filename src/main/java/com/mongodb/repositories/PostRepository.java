ccb0f4974d918a5f9d7a92ab6fc3e7f7dfa9ae36
lauromiottojr <junior.miotto36@gmail.com>
2022-11-30 16:55:31 -0300

    findByTitle using @query

--- src/main/java/com/mongodb/repositories/PostRepository.java
+++ src/main/java/com/mongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.mongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.models.Post;
@@ -11,5 +12,8 @@ import com.mongodb.models.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
