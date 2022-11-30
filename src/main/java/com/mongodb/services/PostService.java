62057137caa973bc043eb16cbd3e67add1280d7c
lauromiottojr <junior.miotto36@gmail.com>
2022-11-30 11:41:55 -0300

    findById Post

---
+++ src/main/java/com/mongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.mongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.models.Post;
+import com.mongodb.repositories.PostRepository;
+import com.mongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!!!"));
+	}
+
+}
