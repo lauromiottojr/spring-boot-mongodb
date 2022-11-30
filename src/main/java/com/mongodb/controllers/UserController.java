671065d5680ae0693c87c45e0abf17b012d1b4fa
lauromiottojr <junior.miotto36@gmail.com>
2022-11-30 11:11:54 -0300

    Endpoint to return User's Posts

--- src/main/java/com/mongodb/controllers/UserController.java
+++ src/main/java/com/mongodb/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mongodb.dtos.UserDTO;
+import com.mongodb.models.Post;
 import com.mongodb.models.User;
 import com.mongodb.services.UserService;
 
@@ -60,8 +61,13 @@ public class UserController {
 		User obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
