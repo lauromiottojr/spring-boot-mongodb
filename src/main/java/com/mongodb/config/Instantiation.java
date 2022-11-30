98e249cfef1e86e2922b2f682e4050bfd341f549
lauromiottojr <junior.miotto36@gmail.com>
2022-11-30 10:42:15 -0300

    Refactoring User Document

--- src/main/java/com/mongodb/config/Instantiation.java
+++ src/main/java/com/mongodb/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mongodb.dtos.AuthorDTO;
 import com.mongodb.models.Post;
 import com.mongodb.models.User;
 import com.mongodb.repositories.PostRepository;
@@ -28,20 +29,21 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		//deleteAll
+		// deleteAll
 		userRepo.deleteAll();
 		postRepo.deleteAll();
-		
-		//new users
+
+		// new users
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
 
-		//new posts
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP!!! ABS", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia!", "Acordei feliz!", maria);
+		// new posts
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP!!! ABS",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia!", "Acordei feliz!", new AuthorDTO(maria));
 
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
